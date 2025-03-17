5c65ad08f977b58a1e354f040098c77f6f9776ea
Julien Marrec <julien.marrec@gmail.com>
2025-03-17 11:09:57 +0100

    Write a unit test for addEnergyplusOutputRequests and mergeOutputTableSummaryReports

--- src/workflow/test/Util_GTest.cpp
+++ src/workflow/test/Util_GTest.cpp
@@ -6,9 +6,19 @@
 #include <gtest/gtest.h>
 
 #include "../Util.hpp"
+#include "../../utilities/idf/IdfFile.hpp"
+#include "../../utilities/idf/IdfExtensibleGroup.hpp"
+#include "../../utilities/idf/Workspace.hpp"
+#include "../../utilities/idf/WorkspaceObject.hpp"
+#include "../../utilities/idf/IdfObject.hpp"
+#include "../../utilities/idd/IddObject.hpp"
+
+#include <utilities/idd/IddEnums.hxx>
 
 #include <map>
 #include <string>
+
+using namespace openstudio;
 
 class WorkflowFixture : public testing::Test
 {
@@ -66,5 +76,117 @@ TEST_F(WorkflowFixture, Util_sanitizeKey) {
     const std::string sanitizedKey = openstudio::workflow::util::sanitizeKey(originalKey);
     // Check if the sanitized key matches the expected value
     EXPECT_EQ(sanitizedKey, expectedKey) << "Error: Key '" << originalKey << "' was not sanitized as expected.";
+  }
+}
+
+TEST_F(WorkflowFixture, Util_mergeOutputTableSummaryReports) {
+  IdfObject existingObject(IddObjectType::Output_Table_SummaryReports);
+  existingObject.pushExtensibleGroup({"InputVerificationandResultsSummary"});
+  existingObject.pushExtensibleGroup({"ClimaticDataSummary"});
+
+  IdfObject newObject(IddObjectType::Output_Table_SummaryReports);
+  newObject.pushExtensibleGroup({"ComponentSizingSummary"});
+  newObject.pushExtensibleGroup({"ClimaticDataSummary"});
+  newObject.pushExtensibleGroup({"EnvelopeSummary"});
+
+  EXPECT_TRUE(openstudio::workflow::util::mergeOutputTableSummaryReports(existingObject, newObject));
+  ASSERT_EQ(4, existingObject.numExtensibleGroups());
+  auto egs = existingObject.extensibleGroups();
+  EXPECT_EQ("InputVerificationandResultsSummary", egs.at(0).getString(0).get());
+  EXPECT_EQ("ClimaticDataSummary", egs.at(1).getString(0).get());
+  EXPECT_EQ("ComponentSizingSummary", egs.at(2).getString(0).get());
+  EXPECT_EQ("EnvelopeSummary", egs.at(3).getString(0).get());
+}
+
+TEST_F(WorkflowFixture, Util_addEnergyPlusOutputRequest) {
+
+  Workspace w(StrictnessLevel::Draft, IddFileType::EnergyPlus);
+  std::vector<IdfObject> energyPlusOutputRequests;
+  {
+    // Unique with explicit merge
+    WorkspaceObject wo = w.addObject(IdfObject(IddObjectType::Output_Table_SummaryReports)).get();
+    wo.pushExtensibleGroup({"InputVerificationandResultsSummary"});
+    wo.pushExtensibleGroup({"ClimaticDataSummary"});
+
+    auto& newObject = energyPlusOutputRequests.emplace_back(IddObjectType::Output_Table_SummaryReports);
+    newObject.pushExtensibleGroup({"ComponentSizingSummary"});
+    newObject.pushExtensibleGroup({"ClimaticDataSummary"});
+    newObject.pushExtensibleGroup({"EnvelopeSummary"});
+
+    EXPECT_TRUE(openstudio::workflow::util::addEnergyPlusOutputRequest(w, newObject));
+    {
+      auto wos = w.getObjectsByType(IddObjectType::Output_Table_SummaryReports);
+      ASSERT_EQ(1, wos.size());
+      auto egs = wos.front().extensibleGroups();
+      EXPECT_EQ("InputVerificationandResultsSummary", egs.at(0).getString(0).get());
+      EXPECT_EQ("ClimaticDataSummary", egs.at(1).getString(0).get());
+      EXPECT_EQ("ComponentSizingSummary", egs.at(2).getString(0).get());
+      EXPECT_EQ("EnvelopeSummary", egs.at(3).getString(0).get());
+    }
+  }
+  {
+    // Unique and present already
+    WorkspaceObject wo = w.addObject(IdfObject(IddObjectType::Output_SQLite)).get();
+    wo.setString(0, "Simple");
+
+    auto& newObject = energyPlusOutputRequests.emplace_back(IddObjectType::Output_SQLite);
+    newObject.setString(0, "SimpleAndTabular");
+    newObject.setString(1, "JtoKWH");
+
+    EXPECT_TRUE(openstudio::workflow::util::addEnergyPlusOutputRequest(w, newObject));
+    {
+      auto wos = w.getObjectsByType(IddObjectType::Output_SQLite);
+      ASSERT_EQ(1, wos.size());
+      const auto& new_wo = wos.front();
+      EXPECT_EQ("SimpleAndTabular", new_wo.getString(0).get());
+      EXPECT_EQ("JtoKWH", new_wo.getString(1).get());
+    }
+  }
+  {
+    // Unique and not present already
+    auto& newObject = energyPlusOutputRequests.emplace_back(IddObjectType::Output_Diagnostics);
+    newObject.setString(0, "DisplayExtraWarnings");
+    newObject.setString(1, "DisplayUnusedSchedules");
+
+    EXPECT_EQ(0, w.getObjectsByType(IddObjectType::Output_Diagnostics).size());
+    EXPECT_TRUE(openstudio::workflow::util::addEnergyPlusOutputRequest(w, newObject));
+    {
+      auto wos = w.getObjectsByType(IddObjectType::Output_Diagnostics);
+      ASSERT_EQ(1, wos.size());
+      const auto& new_wo = wos.front();
+      EXPECT_EQ("DisplayExtraWarnings", new_wo.getString(0).get());
+      EXPECT_EQ("DisplayUnusedSchedules", new_wo.getString(1).get());
+    }
+  }
+  {
+    // Non unique, and present already
+    WorkspaceObject wo = w.addObject(IdfObject(IddObjectType::Schedule_Constant)).get();
+    wo.setString(0, "AlwaysOn");
+    wo.setDouble(2, 1.0);
+
+    auto& newObject = energyPlusOutputRequests.emplace_back(IddObjectType::Schedule_Constant);
+    newObject.setString(0, "AlwaysOn");
+    newObject.setDouble(2, 1.0);
+    EXPECT_FALSE(openstudio::workflow::util::addEnergyPlusOutputRequest(w, newObject));
+    EXPECT_EQ(1, w.getObjectsByType(IddObjectType::Schedule_Constant).size());
+  }
+  {
+    // Non unique, and not present already
+    auto& newObject = energyPlusOutputRequests.emplace_back(IddObjectType::Schedule_Constant);
+    newObject.setString(0, "A New Schedule");
+    newObject.setDouble(2, 18.0);
+    EXPECT_EQ(1, w.getObjectsByType(IddObjectType::Schedule_Constant).size());
+    EXPECT_TRUE(w.getObjectByTypeAndName(IddObjectType::Schedule_Constant, "AlwaysOn"));
+    EXPECT_TRUE(openstudio::workflow::util::addEnergyPlusOutputRequest(w, newObject));
+    {
+      auto wos = w.getObjectsByType(IddObjectType::Schedule_Constant);
+      ASSERT_EQ(2, wos.size());
+      EXPECT_TRUE(w.getObjectByTypeAndName(IddObjectType::Schedule_Constant, "AlwaysOn"));
+      auto new_wo_ = w.getObjectByTypeAndName(IddObjectType::Schedule_Constant, "A New Schedule");
+      ASSERT_TRUE(new_wo_);
+      EXPECT_EQ("A New Schedule", new_wo_->getString(0).get());
+      EXPECT_TRUE(new_wo_->isEmpty(1));
+      EXPECT_EQ(18.0, new_wo_->getDouble(2).get());
+    }
   }
 }
