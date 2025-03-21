1da6dfb9acf48b22007f1254c10bae01e13c7b52
Julien Marrec <julien.marrec@gmail.com>
2025-03-21 18:33:52 +0100

    Similar simple updates to use getOrCreateThermalZoneDSOA

--- src/energyplus/ForwardTranslator/ForwardTranslateAirTerminalSingleDuctVAVReheat.cpp
+++ src/energyplus/ForwardTranslator/ForwardTranslateAirTerminalSingleDuctVAVReheat.cpp
@@ -207,12 +207,9 @@ namespace energyplus {
           auto zones = airLoopHVAC->demandComponents(modelObject, airLoopHVAC->demandOutletNode(), model::ThermalZone::iddObjectType());
           if (!zones.empty()) {
             auto zone = zones.front();
-            auto spaces = zone.cast<model::ThermalZone>().spaces();
-            if (!spaces.empty()) {
-              if (auto designSpecificationOutdoorAir = spaces.front().designSpecificationOutdoorAir()) {
+            if (auto dsoaOrList_  = getOrCreateThermalZoneDSOA(zone.cast<ThermalZone>())) {
                 idfObject.setString(AirTerminal_SingleDuct_VAV_ReheatFields::DesignSpecificationOutdoorAirObjectName,
-                                    designSpecificationOutdoorAir->name().get());
-              }
+                                    dsoaOrList_->nameString());
             }
           }
         }
