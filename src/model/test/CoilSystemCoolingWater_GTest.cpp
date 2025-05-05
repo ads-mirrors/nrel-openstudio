310a14f7df9ccc215ea5e0a67a649c8924aab86f
Julien Marrec <julien.marrec@gmail.com>
2025-05-05 11:45:02 +0200

    Improve model gtests

--- src/model/test/CoilSystemCoolingWater_GTest.cpp
+++ src/model/test/CoilSystemCoolingWater_GTest.cpp
@@ -7,13 +7,18 @@
 
 #include "../CoilSystemCoolingWater.hpp"
 #include "../CoilSystemCoolingWater_Impl.hpp"
-#include "../Schedule.hpp"
-#include "../Schedule_Impl.hpp"
+
 #include "../CoilCoolingWater.hpp"
 #include "../CoilCoolingWater_Impl.hpp"
+#include "../CoilSystemCoolingWaterHeatExchangerAssisted.hpp"
+#include "../CoilSystemCoolingWaterHeatExchangerAssisted_Impl.hpp"
 #include "../AirLoopHVAC.hpp"
-#include "../PlantLoop.hpp"
+#include "../ChillerElectricEIR.hpp"
 #include "../Node.hpp"
+#include "../PlantLoop.hpp"
+#include "../Schedule.hpp"
+#include "../ScheduleConstant.hpp"
+#include "../ScheduleTypeLimits.hpp"
 
 using namespace openstudio;
 using namespace openstudio::model;
@@ -26,35 +31,70 @@ TEST_F(ModelFixture, CoilSystemCoolingWater_GettersSetters) {
   coilSystemCoolingWater.setName("My CoilSystemCoolingWater");
 
   // Availability Schedule Name: Required Object
+  // Ctor default
   Schedule availabilitySchedule = m.alwaysOnDiscreteSchedule();
-  EXPECT_TRUE(coilSystemCoolingWater.setAvailabilitySchedule(availabilitySchedule));
   EXPECT_EQ(availabilitySchedule, coilSystemCoolingWater.availabilitySchedule());
+  // set
+  ScheduleConstant availSch(m);
+  EXPECT_TRUE(coilSystemCoolingWater.setAvailabilitySchedule(availSch));
+  EXPECT_EQ(availSch, coilSystemCoolingWater.availabilitySchedule());
+  // The Schedule Type Registry should have done it's thing and assigned a scheduleTypeLimits
+  ASSERT_TRUE(availSch.scheduleTypeLimits());
+  {
+    auto sch_type_lim = availSch.scheduleTypeLimits().get();
+    EXPECT_EQ("OnOff", sch_type_lim.nameString());
+    ASSERT_TRUE(sch_type_lim.lowerLimitValue());
+    EXPECT_EQ(0.0, sch_type_lim.lowerLimitValue().get());
+    ASSERT_TRUE(sch_type_lim.upperLimitValue());
+    EXPECT_EQ(1.0, sch_type_lim.upperLimitValue().get());
+    ASSERT_TRUE(sch_type_lim.numericType());
+    EXPECT_EQ("Discrete", sch_type_lim.numericType().get());
+    EXPECT_EQ("Availability", sch_type_lim.unitType());
+  }
 
   // Cooling Coil: Required Object
+  // Ctor instantiates a CoilCoolingWater
+  EXPECT_TRUE(coilSystemCoolingWater.coolingCoil().optionalCast<CoilCoolingWater>());
   CoilCoolingWater coolingCoil(m);
   EXPECT_TRUE(coilSystemCoolingWater.setCoolingCoil(coolingCoil));
   EXPECT_EQ(coolingCoil, coilSystemCoolingWater.coolingCoil());
 
+  CoilSystemCoolingWaterHeatExchangerAssisted coilSystemCoolingWaterHeatExchangerAssisted(m);
+  EXPECT_TRUE(coilSystemCoolingWater.setCoolingCoil(coilSystemCoolingWaterHeatExchangerAssisted));
+  EXPECT_EQ(coilSystemCoolingWaterHeatExchangerAssisted, coilSystemCoolingWater.coolingCoil());
+
   // Dehumidification Control Type: Required String
-  EXPECT_TRUE(coilSystemCoolingWater.setDehumidificationControlType("None"));
+  // Ctor default
   EXPECT_EQ("None", coilSystemCoolingWater.dehumidificationControlType());
+  // set
+  EXPECT_TRUE(coilSystemCoolingWater.setDehumidificationControlType("CoolReheat"));
+  EXPECT_EQ("CoolReheat", coilSystemCoolingWater.dehumidificationControlType());
   // Bad Value
   EXPECT_FALSE(coilSystemCoolingWater.setDehumidificationControlType("BADENUM"));
-  EXPECT_EQ("None", coilSystemCoolingWater.dehumidificationControlType());
+  EXPECT_EQ("CoolReheat", coilSystemCoolingWater.dehumidificationControlType());
 
   // Run on Sensible Load: Required Boolean
-  EXPECT_TRUE(coilSystemCoolingWater.setRunonSensibleLoad(true));
+  // Ctor default
   EXPECT_TRUE(coilSystemCoolingWater.runonSensibleLoad());
+  // set
   EXPECT_TRUE(coilSystemCoolingWater.setRunonSensibleLoad(false));
   EXPECT_FALSE(coilSystemCoolingWater.runonSensibleLoad());
+  EXPECT_TRUE(coilSystemCoolingWater.setRunonSensibleLoad(true));
+  EXPECT_TRUE(coilSystemCoolingWater.runonSensibleLoad());
 
   // Run on Latent Load: Required Boolean
+  // Ctor default
+  EXPECT_FALSE(coilSystemCoolingWater.runonLatentLoad());
+  // set
   EXPECT_TRUE(coilSystemCoolingWater.setRunonLatentLoad(true));
   EXPECT_TRUE(coilSystemCoolingWater.runonLatentLoad());
   EXPECT_TRUE(coilSystemCoolingWater.setRunonLatentLoad(false));
   EXPECT_FALSE(coilSystemCoolingWater.runonLatentLoad());
 
   // Minimum Air To Water Temperature Offset: Required Double
+  // Ctor default
+  EXPECT_EQ(0.0, coilSystemCoolingWater.minimumAirToWaterTemperatureOffset());
+  // set
   EXPECT_TRUE(coilSystemCoolingWater.setMinimumAirToWaterTemperatureOffset(1.0));
   EXPECT_EQ(1.0, coilSystemCoolingWater.minimumAirToWaterTemperatureOffset());
   // Bad Value
@@ -62,18 +102,30 @@ TEST_F(ModelFixture, CoilSystemCoolingWater_GettersSetters) {
   EXPECT_EQ(1.0, coilSystemCoolingWater.minimumAirToWaterTemperatureOffset());
 
   // Economizer Lockout: Required Boolean
-  EXPECT_TRUE(coilSystemCoolingWater.setEconomizerLockout(true));
+  // Ctor default
   EXPECT_TRUE(coilSystemCoolingWater.economizerLockout());
+  // set
   EXPECT_TRUE(coilSystemCoolingWater.setEconomizerLockout(false));
   EXPECT_FALSE(coilSystemCoolingWater.economizerLockout());
+  EXPECT_TRUE(coilSystemCoolingWater.setEconomizerLockout(true));
+  EXPECT_TRUE(coilSystemCoolingWater.economizerLockout());
 
   // Minimum Water Loop Temperature For Heat Recovery: Required Double
+  // Ctor default
+  EXPECT_EQ(0.0, coilSystemCoolingWater.minimumWaterLoopTemperatureForHeatRecovery());
+  // set
   EXPECT_TRUE(coilSystemCoolingWater.setMinimumWaterLoopTemperatureForHeatRecovery(1.2));
   EXPECT_EQ(1.2, coilSystemCoolingWater.minimumWaterLoopTemperatureForHeatRecovery());
 
   // Companion Coil Used For Heat Recovery: Optional Object
+  // The only type allowed is CoilCoolingWater
   CoilCoolingWater companionCoilUsedForHeatRecovery(m);
   EXPECT_TRUE(coilSystemCoolingWater.setCompanionCoilUsedForHeatRecovery(companionCoilUsedForHeatRecovery));
+  ASSERT_TRUE(coilSystemCoolingWater.companionCoilUsedForHeatRecovery());
+  EXPECT_EQ(companionCoilUsedForHeatRecovery, coilSystemCoolingWater.companionCoilUsedForHeatRecovery().get());
+
+  CoilSystemCoolingWaterHeatExchangerAssisted wrongCompanionCoil(m);
+  EXPECT_FALSE(coilSystemCoolingWater.setCompanionCoilUsedForHeatRecovery(wrongCompanionCoil));
   ASSERT_TRUE(coilSystemCoolingWater.companionCoilUsedForHeatRecovery());
   EXPECT_EQ(companionCoilUsedForHeatRecovery, coilSystemCoolingWater.companionCoilUsedForHeatRecovery().get());
 }
@@ -81,12 +133,22 @@ TEST_F(ModelFixture, CoilSystemCoolingWater_GettersSetters) {
 TEST_F(ModelFixture, CoilSystemCoolingWater_HeatCoolFuelTypes) {
   Model m;
 
-  CoilSystemCoolingWater coilSystemCoolingWater(m);
+  CoilSystemCoolingWater coilSystem(m);
+  auto cc = coilSystem.coolingCoil().cast<CoilCoolingWater>();
 
-  EXPECT_EQ(ComponentType(ComponentType::Cooling), coilSystemCoolingWater.componentType());
-  testFuelTypeEquality({}, coilSystemCoolingWater.coolingFuelTypes());
-  testFuelTypeEquality({}, coilSystemCoolingWater.heatingFuelTypes());
-  testAppGFuelTypeEquality({}, coilSystemCoolingWater.appGHeatingFuelTypes());
+  EXPECT_EQ(ComponentType(ComponentType::Cooling), coilSystem.componentType());
+  testFuelTypeEquality({}, coilSystem.coolingFuelTypes());
+  testFuelTypeEquality({}, coilSystem.heatingFuelTypes());
+  testAppGFuelTypeEquality({}, coilSystem.appGHeatingFuelTypes());
+
+  PlantLoop p(m);
+  ChillerElectricEIR ch(m);
+  EXPECT_TRUE(p.addSupplyBranchForComponent(ch));
+  EXPECT_TRUE(p.addDemandBranchForComponent(cc));
+  EXPECT_EQ(ComponentType(ComponentType::Cooling), coilSystem.componentType());
+  testFuelTypeEquality({FuelType::Electricity}, coilSystem.coolingFuelTypes());
+  testFuelTypeEquality({}, coilSystem.heatingFuelTypes());
+  testAppGFuelTypeEquality({}, coilSystem.appGHeatingFuelTypes());
 }
 
 TEST_F(ModelFixture, CoilSystemCoolingWater_clone) {}
