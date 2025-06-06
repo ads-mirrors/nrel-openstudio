cf6adddd2c2a19f3cd6464239928aac46b66ddc0
Julien Marrec <julien.marrec@gmail.com>
2025-06-06 14:31:38 +0200

    Add a test for #5421

---
+++ src/cli/test/memleak_source.py
@@ -0,0 +1,12 @@
+import openstudio
+import pytest
+
+def openstudio_import():
+    m = openstudio.model.Model()
+    raise ValueError("Throwing on purpose")
+
+def test_openstudio_import():
+    openstudio_import()
+
+if __name__ == "__main__":
+    pytest.main([__file__, "--capture=no", "--verbose"])
