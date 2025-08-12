802b89056c570b3e366f21fe43f73cedab4b06d6
Julien Marrec <julien.marrec@gmail.com>
2025-08-12 17:21:32 +0200

    Clarify VT test

--- src/osversion/test/3_10_1/test_vt_People.rb
+++ src/osversion/test/3_10_1/test_vt_People.rb
@@ -6,7 +6,8 @@ m = Model.new
 
 people_def = PeopleDefinition.new(m)
 people = People.new(people_def)
-s = OpenStudio::Model::ScheduleConstant.new(m)
-people.setClothingInsulationSchedule(s)
+clo_sch = OpenStudio::Model::ScheduleConstant.new(m)
+clo_sch.setName("Clothing Insulation Schedule")
+people.setClothingInsulationSchedule(clo_sch)
 
 m.save('test_vt_People.osm', true)
